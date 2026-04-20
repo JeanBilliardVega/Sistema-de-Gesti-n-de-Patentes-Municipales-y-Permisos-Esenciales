16b370640240238078e6b764d58d050128da595a
jan28f <hans.silva.g@mail.pucv.cl>
2026-04-20 18:34:40 -0400

    Inicializacion de proyecto Ionic React

---
+++ sistema-patentes-permisos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sistema-patentes-permisos',
+  webDir: 'dist'
+};
+
+export default config;
